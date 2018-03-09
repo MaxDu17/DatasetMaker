aa4673151d36187cd2f0be712ad7b4b6b79fdb00
Max Du <maxdubusiness@gmail.com>
2018-03-09 01:22:47 -0500

    ok I put stuff in

---
+++ MakeUnknown.py
@@ -0,0 +1,76 @@
+import pyaudio
+import sys
+import wave
+import keyboard
+import time
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 4096
+RECORD_SECONDS = 2
+
+inhaleCounter = 0
+exhaleCounter = 0
+unknownCounter = 0
+Time = time.clock()
+life = True
+
+
+def fileName(curr_number):
+
+    result = "dataSPLIT/unknown/" + str(curr_number) + ".wav"
+    return result
+    
+def makeFile(stream, recorder,file_name,frames):
+    stream.stop_stream()
+    stream.close()
+    recorder.terminate()
+
+    wf = wave.open(file_name, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(recorder.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b"".join(frames))
+    wf.close()
+
+
+def record():
+    global unknownCounter
+    global life
+    file_name = ""
+    Time = time.clock()
+    recorder = pyaudio.PyAudio()
+    stream = recorder.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+    
+    frames = []
+    while Time + RECORD_SECONDS > time.clock():
+        data = stream.read(CHUNK)
+        frames.append(data)
+    print("done")
+    file_name = fileName(unknownCounter)
+    unknownCounter += 1
+    makeFile(stream,recorder,file_name,frames)
+
+
+
+if __name__ == '__main__':
+    unknownCounter = int(input("what unknown?\n"))
+    print("starting in 3")
+    time.sleep(1)
+    print("starting in 2")
+    time.sleep(1)
+    print("starting in 1")
+    time.sleep(1)
+    print("live")
+    for i in range(50):
+        if life:
+            record()
+            time.sleep(0.1)
+        else:
+            break
+        
